c8b9f2f218611af7e4011a366587f5b982054d93
crolbar <crolbar@crolbar.com>
2024-04-29 19:00:20 +0300

    flake file for nix dev shell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            cargo-binutils
+            pkg-config
+            dfu-util
+            (rust-bin.stable.latest.default.override {
+              targets = [ "thumbv7em-none-eabihf" ];
+              extensions = [ "rust-src" "llvm-tools-preview" ];
+            })
+          ];
+        };
+      }
+    );
+}
